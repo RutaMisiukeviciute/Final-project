9de9e019048eafa2f0155ac50f4502114c198b67
RutaMisiukeviciute <ruta.misiukeviute@gmail.com>
2024-03-23 10:08:21 +0200

    added footer

--- final-project/src/App.jsx
+++ final-project/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from './components/UI/Header';
+import Footer from './components/UI/Footer';
 
 const App = () => {
   return (
     <>
       <Header />
+      <main></main>
+      <Footer />
     </>
   );
 }
